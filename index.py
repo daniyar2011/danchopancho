233857cd9dcf1d24be44da1f4af5fd429b90efbd
daniyar2011 <daniyarmaratbekov2011@gmail.com>
2024-12-07 23:51:35 +0600

    ну хз чо то с черепашками

---
+++ index.py
@@ -0,0 +1,15 @@
+Black_cage = 'Черепашка 1'
+Red_cage = 'Черепашка 2'
+Green_cage = 'Черепашка 3'
+Yellow_cage = 'Черепашка 4'
+Blue_cage = 'Черепашка 5'
+print(Blue_cage)
+
+Turtle_1 = 400
+Turtle_2 = 470
+Turtle_3 = 390
+Turtle_4 = 130
+Turtle_5 = 431
+
+summ_feed = Turtle_1+Turtle_2+Turtle_3+Turtle_4+Turtle_5
+print(summ_feed)
